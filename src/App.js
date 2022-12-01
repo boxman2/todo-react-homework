e716f8393b9de3ca108abc3165fa67cf9fdc9862
boxman2 <boxman2@naver.com>
2022-12-01 10:34:23 +0900

    홈워크

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [inputs, setInputs] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+      <div className="center">
+        <div>
+          <input
+            type="text"
+            placeholder="입력하세요"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+
+          <button
+            onClick={() => {
+              setInputs([...inputs, input]);
+              setInput("");
+            }}
+          >
+            추가하기
+          </button>
+        </div>
+        <h1>ToDo List</h1>
+      </div>
+      <div className="boxs">
+        {inputs.map((input, i) => {
+          return <Box input={input} key={`input+${i}`} />;
+        })}
+      </div>
     </div>
   );
+}
+
+function Box({ input }) {
+  return <div className="box">{input}</div>;
 }
 
 export default App;
